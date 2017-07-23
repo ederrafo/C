de15d68b395c7ae857ef9f383ab64f34210d6bd0
Eder Rafo <ederrafo@gmail.com>
2017-07-23 15:02:36 -0500

    initial commit

---
+++ hello-world.c
@@ -0,0 +1,20 @@
+/* #include: indica al compilador incluir otro archivo
+	la funcion printf esta definida en el archivo stdio.h */
+#include <stdio.h>
+
+
+/**
+ *main() : funcion principal. Todos los programas C deben tenerla. Se ejecuta primero
+ * int: integer. Al comienzo significa que la funcion devolvera un entero. Este enter se usa para conocer como acabo el programa. Si devuelve 0 indica success, un valor diferente de 0 indica error.
+ * void main(): no necesita devolver ningun valor.
+ */
+int main()
+{
+
+	printf("Hello World!!\n");
+	return 34;
+}
+
+
+
+
